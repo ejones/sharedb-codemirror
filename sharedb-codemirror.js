d9d12b946983898cac7ce67d376d40c06797d057
chanhyeong <chu7825@gmail.com>
2017-08-14 16:26:48 +0900

    compatible with sharedb ver 1.0.0-beta.7

--- sharedb-codemirror.js
+++ sharedb-codemirror.js
@@ -199,7 +199,7 @@ ShareDBCodeMirror.prototype.applyOp = function(op, source) {
     return;
   }
 
-  if (source === CODE_MIRROR_OP_SOURCE) {
+  if (source === true || source === CODE_MIRROR_OP_SOURCE) {
     if (this.verbose) {
       console.log('ShareDBCodeMirror: skipping local op', op);
     }
